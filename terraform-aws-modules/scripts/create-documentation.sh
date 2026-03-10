f72137e8c78ff0b91ca508307ee26cbd5eb7c1cd
Senad  Dizdarević <112484166+senad-d@users.noreply.github.com>
2026-03-10 10:49:41 +0100

    docs(terraform-aws-modules): remove invalid usage code block markers

--- terraform-aws-modules/scripts/create-documentation.sh
+++ terraform-aws-modules/scripts/create-documentation.sh
@@ -71,9 +71,7 @@ cat > "${readme_path}" <<EOF
 
 ## Usage Example
 
-```hcl
 <!-- TODO: Replace this comment and provide a minimal but complete HCL example for using this module -->
-```
 
 ## Architecture Notes
 
